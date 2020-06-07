4a719f300ccd3b287225d61052835d7dc78d8b01
adrian <adrianfernandezj@gmail.com>
2020-06-07 15:28:22 -0300

    176-Creando un segundo context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import Formulario from './components/Fomulario';
+
 import CategoriasProvider from './context/CategoriasContext';
+import RecetasProvider from './context/RecetasContext';
 
 
 function App() {
   return (
    <CategoriasProvider>
+     <RecetasProvider>
       <Header />
       <div className="container mt-5">
         <div className="row">
           <Formulario />
         </div>
       </div>
+      </RecetasProvider>
    </CategoriasProvider>
   );
 }
