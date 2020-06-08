10ccb2156de774a6e8c0be1374b9556af359ef72
adrian <adrianfernandezj@gmail.com>
2020-06-08 18:28:03 -0300

    181-Consultando la API para obtener la Receta en específico

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,21 +1,38 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState} from 'react';
 import axios from 'axios';
 
 // crear el context
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
-    
+
     // state del provider
-    const [idreceta, guardarIdReceta] = useState(null);
-    
+    const [ idreceta, guardarIdReceta ] = useState(null);
+    const [ receta, guardarReceta] = useState({});
+
+    // una vez que tenemos una receta, llamar la api
+    useEffect( () => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+            const resultado = await axios.get(url);
+
+            console.log(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+    }, [idreceta]);
+
     return ( 
         <ModalContext.Provider
             value={{
-                guardarIdReceta
+                receta,
+                guardarIdReceta,
+                guardarReceta
             }}
         >
-          {props.children}  
+            {props.children}
         </ModalContext.Provider>
      );
 }
